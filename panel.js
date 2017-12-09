772885b068ab5accddb92fe491b2ea6e1f54c15c
Nico Hoffmann <on-github@nhoffmann.com>
2017-12-09 09:07:24 +0100

    JS improvements for v3

--- panel.js
+++ panel.js
@@ -8,60 +8,51 @@ panel.field('focus', {
       default: 'Focus'
     }
   },
-  data() {
-    let focusImageUrl = panel.config.index + '/media/pages/' + this.$route.params.path + '/' + this.$route.params.filename;
-    let focusCoordinates = JSON.parse(this.value || '{"x":0.5,"y":0.5}');
+  data () {
+    let coordinates = JSON.parse(this.value || '{"x":0.5,"y":0.5}')
 
     return {
-      focusImageUrl: focusImageUrl,
-      focusPointerStyles: {
-        left: focusCoordinates.x * 100 + '%',
-        top: focusCoordinates.y * 100 + '%'
+      left: coordinates.x,
+      top: coordinates.y,
+      image: null
+    }
+  },
+  computed: {
+    json () {
+      return `{"x":${this.left},"y":${this.top}}`
+    },
+    style () {
+      return {
+        left: `${this.left * 100}%`,
+        top: `${this.top * 100}%`
       }
     }
   },
-  watch: {
-    $route: 'fetchFocusData'
+  created () {
+    this.fetch()
   },
   methods: {
-    fetchFocusData() {
+    fetch () {
       this.$api.file.get(this.$route.params.path, this.$route.params.filename).then((file) => {
-        this.focusImageUrl = file.url + '?v=' + file.modified;
-
-        // #FIXME: focus field key is used fixed
-        focusCoordinates = JSON.parse(file.content.focus || '{"x":0.5,"y":0.5}');
-        this.focusPointerStyles = {
-          left: focusCoordinates.x * 100 + '%',
-          top: focusCoordinates.y * 100 + '%'
-        }
-      });
+        this.image = file.url + '?v=' + file.modified
+      })
     },
     setFocus(event) {
-      if (event) {
-        let offsetXPercentage = Math.round(event.offsetX / event.target.width * 100) / 100;
-        let offsetYPercentage = Math.round(event.offsetY / event.target.height * 100) / 100;
-
-        // field value
-        let pos = {'x':offsetXPercentage, 'y':offsetYPercentage};
-        this.data = JSON.stringify(pos);
-
-        // pointer
-        this.focusPointerStyles.left = offsetXPercentage * 100 + '%';
-        this.focusPointerStyles.top = offsetYPercentage * 100 + '%';
-      }
+      this.left = Math.round(event.offsetX / event.target.width * 100) / 100
+      this.top = Math.round(event.offsetY / event.target.height * 100) / 100
+      this.$emit('input', this.json)
     }
   },
   template: `
-    <kirby-field :label="label" :required="required" :readonly="readonly" :name="name">
-      <div class="kirby-focus">
-        <div class="focus-wrapper">
-          <div id="focus-box" class="focus-box" v-on:click="setFocus">
-            <img class="focus-preview" :src="focusImageUrl">
-            <div class="focus-point" :style="focusPointerStyles"></div>
-          </div>
-        </div>
-        <input readonly class="focus-values" v-model="data">
+    <kirby-field class="kirby-focus-field" :label="label" :required="required" :readonly="readonly" :name="name">
+
+      <div class="focus-box">
+        <img class="focus-preview" :src="image" @click="setFocus" />
+        <div class="focus-point" :style="style"></div>
       </div>
+
+      <input readonly class="focus-values" :value="json">
+
     </kirby-field>
-  `,
+  `
 });
